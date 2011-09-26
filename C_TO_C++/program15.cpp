932e73f0dd33681f28556bebf23f72dfae36fa06
haspeleo <hassen.doghmen@gmail.com>
2011-09-26 17:59:59 +0200

    tip 19

--- C_TO_C++/program15.cpp
+++ C_TO_C++/program15.cpp
@@ -22,12 +22,12 @@ int main () {
 
     int i1 = 34, i2 = 6, i3;
      
-    i3 = minimum (i1, i2);
+    i3 = minimum (i1, i2); // compiler generates int minimum (int a, int b)
     cout << "Most little: "<< i3 <<endl;
 
     double d1 = 7.9, d2 = 32.1, d3;
     
-    d3 = minimum (d1, d2);
+    d3 = minimum (d1, d2); // compiler generates double ( double a, double b)
         
     cout << "Most little:  " << d3 <<endl;
     cout << "Most little: " << minimum (d3, 3.5)<<endl;
