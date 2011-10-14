6b6adea83e69352d8e3f1803a5c67d23c777273f
doghmen <doghmen@dell830.(none)>
2011-10-14 10:46:59 +0200

    R&D

--- LINEAR_PROGRAMMING/problems/giapetto.mod
+++ LINEAR_PROGRAMMING/problems/giapetto.mod
@@ -1,15 +1,17 @@
 /* Variables declaration */
 
+printf "\n Giapetto Problem\n\n";
+
 	var x1 >= 40; 
 	var x2 >= 0;
  
 /* Objective function: Profit */
 
-	maximize Z : 3 * x1 + 2 * x2; 
+	maximize ProfitOfProduction : 3 * x1 + 2 * x2; 
 
 
 /* Constraints */
 
-	s.t.  finishing : 2 * x1 + x2 <= 100;
-	s.t.  carpentry : x1 + x2 <= 80;
+	s.t.  Finishing_constraint  : 2 * x1 + x2 <= 100;
+	s.t.  Carpentry_constraint  : x1 + x2 <= 80;
 end;
--- LINEAR_PROGRAMMING/problems/giapetto.sol
+++ LINEAR_PROGRAMMING/problems/giapetto.sol
@@ -1,17 +1,40 @@
 Problem:    giapetto
-Rows:       1
+Rows:       3
 Columns:    2
-Non-zeros:  2
-Status:     UNDEFINED
-Objective:  Z = 0 (MAXimum)
+Non-zeros:  6
+Status:     OPTIMAL
+Objective:  ProfitOfProduction = 160 (MAXimum)
 
    No.   Row name   St   Activity     Lower bound   Upper bound    Marginal
 ------ ------------ -- ------------- ------------- ------------- -------------
-     1 Z            B              0                             
+     1 ProfitOfProduction
+                    B            160                             
+     2 finishing_constraint
+                    NU           100                         100             2
+     3 carpentry_constraint
+                    B             60                          80 
 
    No. Column name  St   Activity     Lower bound   Upper bound    Marginal
 ------ ------------ -- ------------- ------------- ------------- -------------
-     1 x1           NL             0            40                       < eps
-     2 x2           NL             0             0                       < eps
+     1 x1           NL            40            40                          -1
+     2 x2           B             20             0               
+
+Karush-Kuhn-Tucker optimality conditions:
+
+KKT.PE: max.abs.err. = 0.00e+00 on row 0
+        max.rel.err. = 0.00e+00 on row 0
+        High quality
+
+KKT.PB: max.abs.err. = 0.00e+00 on row 0
+        max.rel.err. = 0.00e+00 on row 0
+        High quality
+
+KKT.DE: max.abs.err. = 0.00e+00 on column 0
+        max.rel.err. = 0.00e+00 on column 0
+        High quality
+
+KKT.DB: max.abs.err. = 0.00e+00 on row 0
+        max.rel.err. = 0.00e+00 on row 0
+        High quality
 
 End of output
