b17445d64154134ccbfeb645779e6551750789a1
doghmen <doghmen@lri5-55.lri.fr>
2011-10-18 15:38:27 +0200

    reading what was written

--- C_TO_C++/program31.cpp
+++ C_TO_C++/program31.cpp
@@ -3,17 +3,31 @@ using namespace std;
 #include <iostream>
 #include <fstream>
 
-
-int main () {
+int main() {
 
     fstream f;
 
     f.open("test.txt", ios::out);
 
-    f << "I write some text in test.txt" <<endl;
+    f << "I write some text in test.txt" << endl;
     double a = 2.71;
 
-    f <<"A value in my file : "<< a <<endl;
+    f << "A value in my file : " << a << endl;
+
+    f.close();
+
+    char c;
+
+    cout << "Let's see what we had written int the file " << endl;
+    cout <<"-------"<<endl;
+
+    f.open("test.txt", ios::in);
+
+    while (!f.eof()) {
+
+        f.get(c);
+        cout << c;
+    }
 
     f.close();
 
