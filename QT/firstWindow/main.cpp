904755a89e7c083c02eb0eb1d3ae138d5adf2cd0
hdoghmen <doghmen@dell830.(none)>
2011-10-24 08:59:36 +0200

    RD-Dev-hpc

---
+++ QT/firstWindow/main.cpp
@@ -0,0 +1,25 @@
+
+
+#include <QApplication>
+#include <QPushButton>
+
+int main ( int argc, char *argv[]) {
+
+    QApplication app (argc, argv);
+//    QWidget fenetre;
+//    fenetre.setFixedSize(400, 250);
+//
+    QPushButton bouton ("Hello, Qt App");
+    //QPushButton bouton ("Hello, Qt App", &fenetre);
+    //    bouton.setFont(QFont("Courrier", 14));
+
+
+    bouton.setText("Pimp my Button !!");
+
+//    bouton.setToolTip("Texte d'aide");
+//    bouton.move(60, 50);
+
+    bouton.show();
+
+    return app.exec();
+}
