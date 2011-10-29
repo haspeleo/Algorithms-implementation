df824dacdaaab4eafc08ddccae7793611d427735
doghmen <doghmen@lri5-55.lri.fr>
2011-10-29 12:23:13 +0200

    customize template stack

--- C++_DESIGN/Pile.h
+++ C++_DESIGN/Pile.h
@@ -9,22 +9,95 @@
 #ifndef PILE_H
 #define	PILE_H
 
+const int MAX = 1024;
+
 template <typename T> class Pile {
 private:
-
     int taille;
-    T element;
+    int nbElements;
+    T *tab;
+    T *tos;
+    void clonage(const Pile&);
+    void destruction();
 
 public:
-    Pile(int taille = 32);
-    ~Pile();
-    void push(T& e);
-    void pull() const;
-    T& top() const;
-    bool isEmpty();
-    bool isFull();
+
+    Pile(int taille = 32) {
+
+        tab = new T[taille];
+        tos = tab - 1;
+        nbElements = 0;
+    }
+
+    Pile& operator=(const Pile& p) {
+	
+	if(this != &p) {
+	    destruction();
+	    clonage(p);
+	}
+	return *this;
+    }
+    
+    ~Pile() {
+
+	destruction();
+    }
+
+    void push(T e) {
+
+        ++tos;
+        *tos = e;
+        ++nbElements;
+    }
+
+    void pull() {
+
+        --tos;
+        --nbElements;
+    }
+
+    T& top() const {
+
+        return *tos;
+    }
+
+    const T& top() {
+
+        return *tos;
+    }
+
+    bool isEmpty() {
+
+        return (nbElements == 0);
+    }
+
+    bool isFull() {
+
+        return (nbElements == MAX);
+    }
 
 };
+
+template <class T>
+void Pile<T>::clonage(const Pile<T> &unePile) {
+    taille = unePile.taille;
+    nbElements = unePile.nbElements;
+
+    tab = new T[taille];
+
+    memcpy(tab, unePile.tab, sizeof (T) * nbElements);
+
+    tos = tab + nbElements - 1;
+}
+
+
+template <class T>
+void Pile<T>::destruction(void) {
+    delete [] tab;
+    tab = 0;
+    tos = 0;
+}
+
 
 #endif	/* PILE_H */
 
