4ffdba84da9db8c169a054b1db3cce1ad4668131
doghmen <doghmen@lri5-55.lri.fr>
2011-11-15 16:57:28 +0100

    format

--- ATM_C++/atm.cc
+++ ATM_C++/atm.cc
@@ -23,182 +23,177 @@
 //</pre><a name="ATM::ATM"><hr><pre>
 
 ATM::ATM(int number, const char * location, Bank & bank)
-  : _number(number),
-    _location(location),
-    _bank(bank),
-    _state(STOPPED),
-    _cardReader(* new CardReader()),
-    _display(* new Display()),
-    _keyboard(* new Keyboard()),
-    _cashDispenser(* new CashDispenser()),
-    _envelopeAcceptor(* new EnvelopeAcceptor),
-    _receiptPrinter(* new ReceiptPrinter()),
-    _operatorPanel(* new OperatorPanel())
-  { }
+: _number(number),
+_location(location),
+_bank(bank),
+_state(STOPPED),
+_cardReader(* new CardReader()),
+_display(* new Display()),
+_keyboard(* new Keyboard()),
+_cashDispenser(* new CashDispenser()),
+_envelopeAcceptor(* new EnvelopeAcceptor),
+_receiptPrinter(* new ReceiptPrinter()),
+_operatorPanel(* new OperatorPanel()) {
+}
 
 
 //</pre><a name="ATM::startupOperation"><hr><pre>
 
-Money ATM::startupOperation()
-  { 
+Money ATM::startupOperation() {
     // Wait for switch to be turned on
-    while (! _operatorPanel.switchOn()) 
+    while (!_operatorPanel.switchOn())
         ;
     _state = RUNNING;
-    return _operatorPanel.getInitialCash(); 
-  }
+    return _operatorPanel.getInitialCash();
+}
 
 //</pre><a name="ATM::serviceCustomers"><hr><pre>
 
-void ATM::serviceCustomers(Money initialCash)
-  {
+void ATM::serviceCustomers(Money initialCash) {
     _cashDispenser.setCash(initialCash);
 
-    while (_state == RUNNING)
-      { 
+    while (_state == RUNNING) {
         CardReader::ReaderStatus readerStatus;
 
         _display.requestCard();
-        do
-          { 
-            if (! _operatorPanel.switchOn())
+        do {
+            if (!_operatorPanel.switchOn())
                 _state = STOPPED;
             else
                 readerStatus = _cardReader.checkForCardInserted();
-          }
-        while (_state == RUNNING && readerStatus == CardReader::NO_CARD);
+        }        while (_state == RUNNING && readerStatus == CardReader::NO_CARD);
         _display.clearDisplay();
-         
-        if (_state == RUNNING)
-          switch (readerStatus)
-            {
-              case CardReader::CARD_HAS_BEEN_READ:      
-                {
-                  Session * session = new Session(_cardReader.cardNumber(),
-                                                  * this,
-                                                  _bank);
-                  session -> doSessionUseCase();
-                  delete session;
 
-                  break;
+        if (_state == RUNNING)
+            switch (readerStatus) {
+                case CardReader::CARD_HAS_BEEN_READ:
+                {
+                    Session * session = new Session(_cardReader.cardNumber(),
+                            * this,
+                            _bank);
+                    session -> doSessionUseCase();
+                    delete session;
+
+                    break;
                 }
 
-              case CardReader::UNREADABLE_CARD:
+                case CardReader::UNREADABLE_CARD:
 
-                _display.reportCardUnreadable();
-                _cardReader.ejectCard();
+                    _display.reportCardUnreadable();
+                    _cardReader.ejectCard();
             }
-      }
-  }
+    }
+}
 
 
 //</pre><a name="ATM::getPIN"><hr><pre>
 
-int ATM::getPIN() const
-  { _display.requestPIN();
+int ATM::getPIN() const {
+    _display.requestPIN();
     int PIN = _keyboard.readPIN(_display);
     _display.clearDisplay();
     return PIN;
-  }
+}
 
 
 //</pre><a name="ATM::getMenuChoice"><hr><pre>
 
 int ATM::getMenuChoice(const char * whatToChoose,
-                       int numItems,
-                       const char * items[]) const
-  { _display.displayMenu(whatToChoose, numItems, items);
-    int choice =  _keyboard.readMenuChoice(numItems);
+        int numItems,
+        const char * items[]) const {
+    _display.displayMenu(whatToChoose, numItems, items);
+    int choice = _keyboard.readMenuChoice(numItems);
     _display.clearDisplay();
     return choice;
-  }
+}
 
 
 //</pre><a name="ATM::getAmountEntry"><hr><pre>
 
-Money ATM::getAmountEntry() const
-  { _display.requestAmountEntry();
+Money ATM::getAmountEntry() const {
+    _display.requestAmountEntry();
     Money amount = _keyboard.readAmountEntry(_display);
     _display.clearDisplay();
     return amount;
-  }
+}
 
 
 //</pre><a name="ATM::checkIfCashAvailable"><hr><pre>
 
-bool ATM::checkIfCashAvailable(Money amount) const
-  { return ! (_cashDispenser.currentCash() < amount);
-  }
+bool ATM::checkIfCashAvailable(Money amount) const {
+    return !(_cashDispenser.currentCash() < amount);
+}
 
 
 //</pre><a name="ATM::dispenseCash"><hr><pre>
 
-void ATM::dispenseCash(Money amount) const
-  { _cashDispenser.dispenseCash(amount);
-  }
+void ATM::dispenseCash(Money amount) const {
+    _cashDispenser.dispenseCash(amount);
+}
 
 
 //</pre><a name="ATM::acceptEnvelope"><hr><pre>
 
-bool ATM::acceptEnvelope() const
-  { return _envelopeAcceptor.acceptEnvelope(); 
-  }
+bool ATM::acceptEnvelope() const {
+    return _envelopeAcceptor.acceptEnvelope();
+}
 
 
 //</pre><a name="ATM::issueReceipt"><hr><pre>
 
 void ATM::issueReceipt(int cardNumber,
-                       int serialNumber,
-                       const char * description,
-                       Money amount,
-                       Money balance,
-                       Money availableBalance) const
-  { 
+        int serialNumber,
+        const char * description,
+        Money amount,
+        Money balance,
+        Money availableBalance) const {
     _receiptPrinter.printReceipt(_number, _location, cardNumber, serialNumber,
-                                 description, amount,
-                                 balance, availableBalance);;
-  }
+            description, amount,
+            balance, availableBalance);
+    ;
+}
 
 
 //</pre><a name="ATM::reEnterPIN"><hr><pre>
 
-int ATM::reEnterPIN() const
-  { _display.requestReEnterPIN();
+int ATM::reEnterPIN() const {
+    _display.requestReEnterPIN();
     int PIN = _keyboard.readPIN(_display);
     _display.clearDisplay();
     return PIN;
-  }
+}
 
 
 //</pre><a name="ATM::reportTransactionFailure"><hr><pre>
 
-bool ATM::reportTransactionFailure(const char * explanation) const
-  {
+bool ATM::reportTransactionFailure(const char * explanation) const {
     _display.reportTransactionFailure(explanation);
     int response = _keyboard.readMenuChoice(2);
     _display.clearDisplay();
     return response == 1;
-  }
+}
 
 
 //</pre><a name="ATM::ejectCard"><hr><pre>
 
-void ATM::ejectCard() const
-  { _cardReader.ejectCard(); }
+void ATM::ejectCard() const {
+    _cardReader.ejectCard();
+}
 
 
 //</pre><a name="ATM::retainCard"><hr><pre>
 
-void ATM::retainCard() const
-  { _cardReader.retainCard(); 
+void ATM::retainCard() const {
+    _cardReader.retainCard();
     _display.reportCardRetained();
     // The above routine will sleep 20 seconds to let user see message
     _display.clearDisplay();
-  }
+}
 
 //</pre><a name="ATM::number"><hr><pre>
 
-int ATM::number() const
-  { return _number; }
+int ATM::number() const {
+    return _number;
+}
 
 //</pre></body></html>
--- ATM_C++/atm.h
+++ ATM_C++/atm.h
@@ -10,63 +10,65 @@
  */
 
 //</pre><a name="ATM"><hr><h3>Interface for class ATM</h3><pre>
-class ATM
-  {
+
+class ATM {
     /* 
      * PURPOSE: Manage the overall ATM and its component parts
      */
 
-    public:
+public:
 
-        ATM(int number, const char * location, Bank & bank);
+    ATM(int number, const char * location, Bank & bank);
 
-        // Start up operation
+    // Start up operation
 
-        Money startupOperation();
+    Money startupOperation();
 
-        // Run the ATM, servicing customers, until the switch is turned off
+    // Run the ATM, servicing customers, until the switch is turned off
 
-        void serviceCustomers(Money initialCash);
+    void serviceCustomers(Money initialCash);
 
-        // Interact with the customer in support of various use cases
+    // Interact with the customer in support of various use cases
 
-        int getPIN() const;
-        int getMenuChoice(const char * whatToChoose,
-                          int numItems,
-                          const char * items[]) const;
-        Money getAmountEntry() const;           // From keyboard - any amount
-        bool checkIfCashAvailable(Money amount) const;
-        void dispenseCash(Money amount) const;
-        bool acceptEnvelope() const;            // True iff success
-        void issueReceipt(int cardNumber,
-                          int serialNumber,
-                          const char * description,
-                          Money amount,
-                          Money balance,
-                          Money availableBalance) const;
-        int reEnterPIN() const;
-        bool reportTransactionFailure(const char * explanation) const;
-        void ejectCard() const;
-        void retainCard() const;
-        
-        int number() const;
+    int getPIN() const;
+    int getMenuChoice(const char * whatToChoose,
+            int numItems,
+            const char * items[]) const;
+    Money getAmountEntry() const; // From keyboard - any amount
+    bool checkIfCashAvailable(Money amount) const;
+    void dispenseCash(Money amount) const;
+    bool acceptEnvelope() const; // True iff success
+    void issueReceipt(int cardNumber,
+            int serialNumber,
+            const char * description,
+            Money amount,
+            Money balance,
+            Money availableBalance) const;
+    int reEnterPIN() const;
+    bool reportTransactionFailure(const char * explanation) const;
+    void ejectCard() const;
+    void retainCard() const;
 
-    private:
+    int number() const;
 
-        int                     _number;
-        const char *            _location;
-        Bank &                  _bank;
-     
-        enum {RUNNING, STOPPED} _state;
+private:
 
-        CardReader &            _cardReader;
-        Display &               _display;
-        Keyboard &              _keyboard;
-        CashDispenser &         _cashDispenser;
-        EnvelopeAcceptor&       _envelopeAcceptor;
-        ReceiptPrinter &        _receiptPrinter;
-        OperatorPanel &         _operatorPanel;
-  };
+    int _number;
+    const char * _location;
+    Bank & _bank;
+
+    enum {
+        RUNNING, STOPPED
+    } _state;
+
+    CardReader & _cardReader;
+    Display & _display;
+    Keyboard & _keyboard;
+    CashDispenser & _cashDispenser;
+    EnvelopeAcceptor& _envelopeAcceptor;
+    ReceiptPrinter & _receiptPrinter;
+    OperatorPanel & _operatorPanel;
+};
 
 
 //</pre></body></html>
