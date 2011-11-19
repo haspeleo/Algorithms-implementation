4717328a390009a464b02784afd5f3a8e2710ec6
doghmen <doghmen@MBP-tao.(none)>
2011-11-19 14:42:01 +0100

    readme

--- DB/sdzMysql/helloWold.cpp
+++ DB/sdzMysql/helloWold.cpp
@@ -1,4 +1,6 @@
+//#include <sqlstring.h>
 #include <mysql++.h>
+
 #include <iostream>
 
 using namespace std;
@@ -9,3 +11,6 @@ int main()
     std::cout << greeting << std::endl;
     return 0;
 }
+
+
+//g++ -I/usr/local/mysql -I./mysql-connector-c++-1.1.0-osx10.5-x86-32bit/include -L/usr/local/lib -lmysqlpp -lmysqlclient helloWold.cpp -o hello 
