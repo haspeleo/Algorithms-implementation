24c7efb903bb5730c5e32fbf85e759cc2cbe1213
haspeleo <doghmen@serveur-mash.(none)>
2011-12-13 19:44:39 +0100

    factory method used in mock testing

---
+++ CSHARP/FactoryPattern/FactoryPattern/MainApp.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FactoryPattern {
+
+    class MainApp {
+
+        static void Main(string[] args) {
+
+            // An array of creators
+            Creator[] creators = new Creator[2];
+            creators[0] = new ConcreteCreatorA();
+            creators[1] = new ConcreteCreatorB();
+
+            // Iterate over creators and create products
+
+            foreach (Creator creator in creators) {
+                Product product = creator.FactoryMethod();
+                Console.WriteLine("Created {0}", product.GetType().Name);
+
+            }
+            //wait for user input
+            Console.Read();
+        }
+    }
+
+}
